dc3e4bb5e1c2c0a2419263ca760773b4796092cf
Naomi Quartsin <naomi.quartsin@kcl.ac.uk>
2023-10-02 17:37:11 +0100

    Add code to frequencies.py

--- primes.py
+++ primes.py
@@ -3,4 +3,11 @@
 
 def primes(number_of_primes):
     list = []
+    for i in range (2,number_of_primes):
+        prime = True
+        for j in range (2,i):
+            if (i%j == 0):
+                prime = False
+        if (prime == True):
+            list.append(i)
     return list
