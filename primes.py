5adac050512d451d34c22c3e183b4be07450a783
Naomi Quartsin <naomi.quartsin@kcl.ac.uk>
2023-10-02 22:34:27 +0100

    Fix bugs aand logic error

--- primes.py
+++ primes.py
@@ -3,11 +3,15 @@
 
 def primes(number_of_primes):
     list = []
-    for i in range (2,number_of_primes):
+    counter = 0
+    i = 2
+    while(counter < number_of_primes):
         prime = True
         for j in range (2,i):
             if (i%j == 0):
                 prime = False
         if (prime == True):
             list.append(i)
+            counter = counter + 1
+        i = i + 1
     return list
